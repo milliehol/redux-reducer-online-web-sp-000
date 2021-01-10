25a10c172961b489f38faa90ceace150062cd0cf
Carla Holcomb <carla.programmer@gmail.com>
2021-01-10 19:42:31 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'INCREASE':
+      return {count: state.count + 1}
+    case 'DECREASE':
+      return {count: state.count - 1}
+    default:
+      return state;
 }
+
